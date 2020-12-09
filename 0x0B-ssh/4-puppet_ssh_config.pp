6fe49538e1663fd4a9bd9a8d120ae2dc596c02fd
JParrales <josedparrales@gmail.com>
2020-12-09 10:48:28 -0500

    folders ssh

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # Create ssh config in ~/.ssh/config on client
 file_line { 'Private_Key_puppet':
   ensure => 'present',
-  path   => '.ssh/ssh_config',
+  path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/holberton',
 }
 
 file_line { 'refuse_password':
   ensure => 'present',
-  path   => '.ssh/ssh_config',
+  path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
 }
