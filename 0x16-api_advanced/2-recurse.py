cdc0cd0e9a38edf6f5303f1f2b14c66127dcffd0
JParrales <josedparrales@gmail.com>
2021-02-04 21:54:58 -0500

    recursions

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python3
 """Reddit client"""
+import pprint
 import requests
 
+BASE_URL = 'http://reddit.com/r/{}/hot.json'
 
-def recurse(subreddit, hot_list=[], after=""):
-    """Querying Reddit API, and returns all
-    hot articles for a given subreddit."""
 
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit, after)
-    headers = {'User-Agent': 'JParrales'}
-    arg = {"after": after}
-    resp = requests.get(url, params=arg, headers=headers)
-    list_a = resp.json().get('data', {}).get('children', None)
-    pagination = resp.json().get('data', {}).get('after', None)
-
-    if pagination is not None:
-
-        if list_a:
-            for item in list_a:
-                hot_list.append(item.get("data").get("title"))
-
-        if pagination is not None:
-            recurse(subreddit, hot_list, pagination)
-
-        return hot_list
-
-    else:
+def recurse(subreddit, hot_list=[], after=None):
+    '''Get ALL hot posts'''
+    headers = {'User-agent': 'JParrales'}
+    params = {'limit': 100}
+    if isinstance(after, str):
+        if after != "STOP":
+            params['after'] = after
+        else:
+            return hot_list
+    response = requests.get(BASE_URL.format(subreddit),
+                            headers=headers, params=params)
+    if response.status_code != 200:
         return None
+    data = response.json().get('data', {})
+    after = data.get('after', 'STOP')
+    if not after:
+        after = "STOP"
+    hot_list = hot_list + [post.get('data', {}).get('title')
+                           for post in data.get('children', [])]
+    return recurse(subreddit, hot_list, after)
